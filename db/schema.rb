3afb1881fb743e0c2cf8b170bf93e696b0fe6663
Kyle Vermeulen <kyletvermeulen@gmail.com>
2018-01-29 22:15:54 +0000

    Implemented concern for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124183517) do
+ActiveRecord::Schema.define(version: 20180129214855) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180124183517) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.text     "badge"
   end
 
   create_table "topics", force: :cascade do |t|
