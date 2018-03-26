81da3a4aeed1110e9412e6f5900cee56908266e2
Kyle Vermeulen <kyletvermeulen@gmail.com>
2018-03-26 21:39:26 +0000

    Implemented initial file uploading system via carrierwave for portfolio items and re did from last revert

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -69,6 +69,8 @@ def portfolio_params
     params.require(:portfolio).permit(:title, 
                                       :subtitle, 
                                       :body, 
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
                                       )
     
