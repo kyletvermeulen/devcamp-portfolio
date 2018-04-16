f78ba2be9cfe6256d6b6464acd445548dd81a705
Kyle Vermeulen <kyletvermeulen@gmail.com>
2018-04-16 21:40:05 +0000

    Implemented the blog widget for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,8 @@ class Topic < ActiveRecord::Base
     validates_presence_of :title
     
     has_many :blogs
+    
+    def self.with_blogs
+       includes(:blogs).where.not(blogs: { id: nil}) 
+    end    
 end
