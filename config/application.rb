8d4088b34ef2d7944031c248fde2b2c4c3ab8d8f
Kyle Vermeulen <kyletvermeulen@gmail.com>
2018-04-25 19:46:52 +0000

    updated secret base call

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
